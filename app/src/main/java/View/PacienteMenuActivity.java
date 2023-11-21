16da75cdb2f1b93c79aa88c8816d3dafa5955158
Diogo <diogosardagna123@gmail.com>
2023-11-21 01:39:29 -0300

    Login e Cadastro: Diogo

---
+++ app/src/main/java/View/PacienteMenuActivity.java
@@ -0,0 +1,16 @@
+package View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartnutri.R;
+
+public class PacienteMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paciente_menu);
+    }
+}
